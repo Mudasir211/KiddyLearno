4e74491bf35de0b24bebef2fb6085704effde8f6
Mudasir211 <mudasir8482289@gmail.com>
2025-08-28 18:37:22 +0500

    .

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -16,7 +16,7 @@ export const metadata = {
   title: "Kiddy-Learno",
   description: "KiddyLearno is a playful, safe online learning platform for kids in *KS1 to KS3(Grade 1 to 8)*. It focuses on core academic support, homework help, and test preparation in a fun, engaging way designed for young learners.Having Affordable Pricing Plans",
    icons: {
-    icon: '/favicon.png', // Or .png or .svg
+    icon: '/favicon.ico?v=2', // Or .png or .svg
   },
 };
 
